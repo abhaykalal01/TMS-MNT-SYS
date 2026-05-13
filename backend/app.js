16ab811c75a590f65c7ce6213a1e1181f9488978
abhay_kalal01 <abhaykalal7@gmail.com>
2026-05-13 12:22:08 +0530

    chore: remove unused taskRoutes import from app.js

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
-<<<<<<< HEAD
 import userRoutes from "./routes/user.routes.js";
 
 import errorMiddleware from "./middleware/error.middleware.js";
-=======
-import taskRoutes from "./routes/task.routes.js";
->>>>>>> 2776801 (feat: implement task management functionality with CRUD operations and validation)
 
 const app = express();
 
